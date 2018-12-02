a94b481350514e31fb45baeabde258bd9d1b6ace
gihankarunarathne <gckarunarathne@gmail.com>
2018-12-03 00:00:13 +0400

    Implemented callback after data transformation complete

--- web/api/aggregate_accumulative.py
+++ web/api/aggregate_accumulative.py
@@ -1,9 +1,10 @@
+import requests
 from flask import Blueprint, request, jsonify, json
-from sqlalchemy import text as sql
 
 from web import util
 
 bp = Blueprint('aggregate_accumulative', __name__)
+callback_url = f'{util.HOST_URL}/extension/transformation/aggregate-accumulative'
 
 """ Transformation Structure:
 {
@@ -27,11 +28,15 @@ bp = Blueprint('aggregate_accumulative', __name__)
         }
     ],
     options: {},
-    from: "",
-    to: "",
-    token: ""
+    callback: "",
+
+    start: "",
+    end: "",
+    token: "",
 }
 """
+
+
 @bp.route('/extension/transformation/aggregate-accumulative', methods=['POST'])
 def extension_transformation_aggregate_accumulative():
     extension = request.get_json()
@@ -42,14 +47,44 @@ def extension_transformation_aggregate_accumulative():
     assert 'outputVariables' in extension and isinstance(extension['outputVariables'], list), \
         f'outputVariables should be provided'
 
-    extension['outputVariables'] = process_aggregate_accumulative(**extension)
+    trigger_data = {
+        "output_variables": extension['outputVariables'],
+        "input_variables": extension['inputVariables'],
+        "options": extension['options'],
+        'callback': extension['callback'],
+        "token": request.args.get('token'),
+        "start": request.args.get('from'),
+        "end": request.args.get('end'),
+    }
+    process_aggregate_accumulative(**trigger_data)
+
     del extension['inputVariables']
-    del extension['options']
+    del extension['outputVariables']
+    extension['callback'] = f'{util.HOST_URL}/extension/transformation/aggregate-accumulative',
     return jsonify(extension)
 
 
-def process_aggregate_accumulative(inputVariables: list = [], outputVariables: list = [], options: object = {}, **kwargs):
-    input_x = inputVariables[0]
-    output_y = outputVariables[0]
+def process_aggregate_accumulative(input_variables=None, output_variables=None, options=None, **kwargs):
+    if options is None:
+        options = {}
+    if output_variables is None:
+        output_variables = []
+    if input_variables is None:
+        input_variables = []
+    input_x = input_variables[0]
+    output_y = output_variables[0]
     output_y['data'] = input_x['data']
+    trigger_callback([output_y], kwargs.get('callback'), kwargs.get('token'))
     return [output_y]
+
+
+def trigger_callback(output_variables, callback, token):
+    print("Trigger callback", output_variables)
+    callback_trigger = {
+        "outputVariables": output_variables,
+        "callback": callback_url,
+    }
+    res = requests.post(f'{callback}/{token}', json=callback_trigger)
+    print(res.status_code, res.text)
+    assert res.status_code is 200, f'Unable to update job completion token: {token} for {callback}'
+    return
--- web/util.py
+++ web/util.py
@@ -1,1 +1,3 @@
+
+HOST_URL = 'http://transformation-aggregate-accumulative.default.svc.cluster.local'
 METADATA_ADAPTER_URL = 'http://adapter-metadata.default.svc.cluster.local'
